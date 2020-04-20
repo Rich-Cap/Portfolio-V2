be44ca90cdaac1c18048af0d91b20e07c61bb446
Rich Capuy <rccapuy@gmail.com>
2020-04-20 00:34:32 -0400

    Images of Projects

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 	// $("button").click(function () {
 	// 	$("#cards-container").slideUp(2000);
 	// });
-	$("#arrowdown").click(function() {
+	$("#arrowdown").click(function () {
 		$('html, body').animate({
 			scrollTop: $("#cards-container").offset().top
 		}, 3000);
@@ -10,5 +10,17 @@ $(document).ready(function () {
 		$(".navbar").addClass("navbar-dark");
 		$(".navbar").delay("slow").fadeIn().css("background-color", "darkblue");
 		// $(".navbar").animate({backgroundColor: '#000'}, 'slow');
+		// var y = $(window).scrollTop();
+		// if (y == "0"){
+		// 	$(".navbar").removeClass("navbar-dark");
+		// 	$(".navbar").addClass("navbar-light");
+		// };
+		$(window).scroll(function() {
+			var height = $(document).scrollTop();
+		
+			if(height < 5) {
+				console.log(height)
+			}
+		});
 	});
 });
